74bbf2291ff1f5495fef903b9cc2a1c60e9c5aa7
Ronan Avery <rta114@uclive.ac.nz>
2021-05-24 12:03:55 +1200

    Push

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,16 +1,42 @@
 <template>
   <el-main id="main">
-    <el-row>
-      <el-col :span="10">
-        <div class="grid-content">dfthed</div>
-      </el-col>
-    </el-row>
+    <el-container>
+      <el-main>
+        <div :v-model="event" class="grid-content">{{event}}</div>
+      </el-main>
+      <el-aside width="150px">
+        <el-scrollbar height="400px">
+          Similiar events
+        </el-scrollbar>
+      </el-aside>
+    </el-container>
   </el-main>
 </template>
 
 <script>
 export default {
-name: "Event.vue"
+  name: "Event.vue",
+  data() {
+    return {
+      eventId: '',
+      event: null,
+    }
+  },
+  mounted() {
+    let eventId = this.$route.params.id
+    this.getEventInfo(eventId);
+  },
+  methods: {
+    async getEventInfo(eventId) {
+      this.eventId = eventId;
+      console.log(99);
+      await this.axios.get('http://localhost:4941/api/v1/events/' + this.eventId)
+        .then((response) => {
+          this.event = response.data;
+          console.log(this.event);
+        })
+    }
+  }
 }
 </script>
 
@@ -19,22 +45,16 @@ name: "Event.vue"
   font-family: 'Ubuntu', sans-serif;
 
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: 3fr 1fr;
   grid-template-rows: auto auto;
   grid-row-gap: 1em;
-
   max-width: 95%;
+  height: 600px;
   background-color: white;
   margin: 1em auto;
   padding: 0.5em 0 0.5em 0;
   border-radius: 20px;
   border: 2px solid rgba(0, 0, 0, 0.02);
   box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .15);
-}
-.el-row {
-  margin-bottom: 20px;
-&:last-child {
-   margin-bottom: 0;
- }
 }
 </style>
