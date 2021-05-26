d0b7e7c885658f2b526ecf0bc0dee2ed31251cbf
Ronan Avery <rta114@uclive.ac.nz>
2021-05-27 11:02:00 +1200

    Last days

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <el-header>
-    Events Site
+  <el-header height="auto" style="background: #1F74FF">
+    <el-container>
+      <el-aside width="80%">
+        <div style="font-size: 30px; font-family: Helvetica; color: white; text-align: center; margin-top: 20px">
+          EVENTSY
+        </div>
+      </el-aside>
+      <el-main style="display: grid; grid-template-columns: auto auto;">
+        <el-button align="right">
+          Register
+        </el-button>
+        <el-button align="right">
+          Login
+        </el-button>
+      </el-main>
+    </el-container>
   </el-header>
   <div id="app">
     <router-view></router-view>
--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="main">
-    <el-container style="width: 100%">
+    <el-container style="width: 100%" v-model="event">
         <el-header height="20px">
           <div :v-if="event != null" :v-model="event" class="grid-content" id="title">{{event.title}}</div>
           <div :v-if="event != null" :v-model="event" class="grid-content" style="text-align: center">{{ event.date }}</div>
@@ -9,15 +9,11 @@
           <el-divider></el-divider>
           <el-container>
             <el-aside>
-              <el-image :src="'http://localhost:4941/api/v1/events/' + event.id + '/image'" fit="contain" style="width: 300px; height: 300px">
-                <template #error>
-                  <div class="image-slot">
-                    <i class="el-icon-picture-outline">No Image</i>
-                  </div>
-                </template>
-              </el-image>
+              <el-image :src="'http://localhost:4941/api/v1/events/' + event.id + '/image'" fit="contain" style="width: 300px; height: 300px">              </el-image>
               <div>
-                sfbs
+                <div v-for="cat in filteredCategories" v-bind:key="cat">
+                  <el-tag size="medium" style="width: auto; margin: 5px">{{cat}}</el-tag>
+                </div>
               </div>
             </el-aside>
             <el-main>
@@ -46,12 +42,7 @@
             <el-container>
               <el-aside width="50%">
                 <el-col>
-                <el-image align="left" :src="'http://localhost:4941/api/v1/users/' + (event.organizerId) + '/image'" fit="fill" style="width: 100px; height: 100px; border: thin solid #DCDFE6; border-radius: 5px">
-                  <template #error>
-                    <div class="image-slot">
-                      <em class="el-icon-picture-outline">No Image</em>
-                    </div>
-                  </template>
+                <el-image align="left" :src="'http://localhost:4941/api/v1/users/' + (event.organizerId) + '/image'" onerror="this.src = 'https://thepeakid.com/wp-content/uploads/2016/03/default-profile-picture.jpg'" fit="fill" style="width: 100px; height: 100px; border: thin solid #DCDFE6; border-radius: 5px">
                 </el-image>
                 </el-col>
                 <el-col style="margin: 10px;">
@@ -64,19 +55,61 @@
                   Accepted Attendees: {{event.attendeeCount}}
                 </el-row>
                 <el-row>
-                  <el-button type="primary">
+                  <el-button type="primary" @click="attendeesVisible = true">
                     View Event Attendees
                   </el-button>
                 </el-row>
               </el-main>
             </el-container>
           </div>
+          <el-dialog
+              title="Attendees"
+              v-model="attendeesVisible"
+              width="50%"
+              :before-close="handleClose">
+            <div>
+                <el-main v-model="attendees">
+                  <el-row v-for="attendee in attendees" v-bind:key="attendee" style="border: thin solid #DCDFE6; border-radius: 5px; margin: 10px">
+                      <el-image style="width: 100px; height: 100px; border: thin solid #DCDFE6; border-radius: 5px; margin: 5px" :src="'http://localhost:4941/api/v1/users/' + (attendee.attendeeId) + '/image'" onerror="this.src = 'https://thepeakid.com/wp-content/uploads/2016/03/default-profile-picture.jpg'">
+                      </el-image>
+                    <div style="margin: 10px;">
+                      {{attendee.firstName + ' ' + attendee.lastName}}
+                    </div>
+                    <el-tag v-model="event" style="margin: 10px; float: right" v-if="attendee.attendeeId  !== event['organizerId']">
+                      attendee
+                    </el-tag>
+                    <el-tag  type=warning v-model="event" style="margin: 10px; float: right" v-if="attendee.attendeeId  === event['organizerId']">
+                      organizer
+                    </el-tag>
+                    <el-tag v-model="event" style="margin: 10px; float: right">
+                      <div>
+                        {{attendee.status}}
+                      </div>
+                    </el-tag>
+                  </el-row>
+                </el-main>
+            </div>
+            <template #footer>
+              <span class="dialog-footer">
+                <el-button @click="attendeesVisible = false">Close</el-button>
+              </span>
+            </template>
+          </el-dialog>
         </el-main>
     </el-container>
-    <div style="border: thin solid #DCDFE6; border-radius: 5px; width: 90%; margin: 5px;">
-        <el-scrollbar height="100%" style="margin: 10px">
-          <!-- Todo similiar events -->
-          ss
+    <div style="border: thin solid #DCDFE6; border-radius: 5px; width: 90%; margin: 5px;" v-if="similiarEvents">
+        <el-scrollbar height="100%" style="margin: 10px" v-model="similiarEvents">
+          <el-header height="5%" style="font-family: Helvetica; font-size: 20px; font-weight: bold; text-align: center; margin: 10px">Similar Events</el-header>
+          <el-divider></el-divider>
+          <el-row v-for="event1 in similiarEvents" :key="event1" style="border: thin solid #DCDFE6; border-radius: 5px; margin: 10px; align-content: center">
+            <el-row>
+              {{event1['title']}}
+            </el-row>
+            <el-row>
+            <el-image fit="contain" style="width: 100px; height: 100px; border: thin solid #DCDFE6; border-radius: 5px; margin: 5px" :src="'http://localhost:4941/api/v1/events/' + (event1.eventId) + '/image'" onerror="this.src = 'https://thepeakid.com/wp-content/uploads/2016/03/default-profile-picture.jpg'">
+            </el-image>
+            </el-row>
+          </el-row>
         </el-scrollbar>
     </div>
   </div>
@@ -90,24 +123,79 @@ export default {
     return {
       eventId: '',
       event: {},
+      attendeesVisible: false,
+      attendees: [],
+      categories: [],
+      filteredCategories: [],
+      similiarEvents: []
     }
   },
   mounted() {
-    let eventId = this.$route.params.id
-    this.getEventInfo(eventId);
+    this.eventId = this.$route.params.id
+    this.getEventInfo();
+    this.getEventCategories();
+    this.getEventAttendees();
+    this.getSimiliarEvents();
+    // localStorage.getItem()
   },
   methods: {
-    async getEventInfo(eventId) {
-      this.eventId = eventId;
+    async getEventInfo() {
       await this.axios.get('http://localhost:4941/api/v1/events/' + this.eventId)
         .then((response) => {
           this.event = response.data;
           console.log(this.event);
           this.event["date"] = moment(this.event["date"]).format('h:mm a MMMM dS yyyy')
         })
+    },
+    handleClose(done) {
+      done();
+      this.attendeesVisible = false
+    },
+    async getEventAttendees() {
+      console.log(localStorage.getItem('auth'))
+      if (localStorage.getItem('auth')) {
+        console.log('loggedin');
+      } else {
+        await this.axios.get('http://localhost:4941/api/v1/events/' + this.eventId + '/attendees')
+            .then((res) => {
+              this.attendees = res.data;
+              console.log(this.attendees);
+            });
+      }
+
+    },
+    async getEventCategories() {
+      await this.axios.get('http://localhost:4941/api/v1/events/categories')
+      .then((res) => {
+        this.categories = res.data;
+        for (let cat in this.categories) {
+          for (let i=0; i<this.event['categories'].length; i++)
+          if (this.categories[cat]['id'] === this.event['categories'][i]) {
+            this.filteredCategories.push(this.categories[cat]['name'])
+          }
+        }
+      })
+    },
+    async getSimiliarEvents() {
+      await this.axios.get('http://localhost:4941/api/v1/events')
+          .then((response) => {
+            let events = response.data;
+            events.reverse();
+            events = events.filter(event => new Date(event.date) >= Date.now());
+            events.map(item => {
+              item.date = moment(item.date).format("DD-MM-YYYY HH:mm");
+            });
+            for (let item in events) {
+              let list = events[item]['categories'].filter(value => this.event['categories'].includes(value));
+              if (list.length > 0 && this.event['title'] !== events[item]['title']) {
+                this.similiarEvents.push(events[item])
+              }
+            }
+            console.log(this.similiarEvents)
+          })
+    },
     }
   }
-}
 </script>
 
 <style scoped>
